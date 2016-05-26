fd3dbda265cfd06bc3b3d8afabaeb2d20d0c134a
infinitearray <bandiharshav97@gmail.com>
2016-05-26 14:48:47 +0530

    Add description bar for contact images

---
+++ js/anim.js
@@ -0,0 +1,21 @@
+function onHover()
+{
+    if($("#im1").is(":hover")) {
+       document.getElementById('description').innerHTML="E-12, OBH - Palash Nivas, IIIT Hyderabad, Gachibowli, Hyderabad-500032";
+    }
+    else if ($("#im2").is(":hover")) {
+      document.getElementById('description').innerHTML="facebook.com/harshavardhan.bandi.1";
+    }
+    else if ($("#im3").is(":hover")) {
+      document.getElementById('description').innerHTML="bandiharshav97@gmail.com, harsha.bandi97@students.iiit.ac.in";
+    }
+    else if ($("#im4").is(":hover")) {
+      document.getElementById('description').innerHTML="twitter.com/harsha_bandi";
+    }
+    else if ($("#im5").is(":hover")) {
+      document.getElementById('description').innerHTML="github.com/infinitearray";
+    }
+    else if ($("#im6").is(":hover")) {
+      document.getElementById('description').innerHTML="bitbucket.org/infinitearray";
+    }
+};
