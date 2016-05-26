66e3fd3af60e62b23104711cde29ae81d00852e1
infinitearray <bandiharshav97@gmail.com>
2016-05-26 15:12:05 +0530

    Fromat the descriptions

--- js/anim.js
+++ js/anim.js
@@ -1,13 +1,13 @@
 function onHover()
 {
     if($("#im1").is(":hover")) {
-       document.getElementById('description').innerHTML="E-12, OBH - Palash Nivas, IIIT Hyderabad, Gachibowli, Hyderabad-500032";
+       document.getElementById('description').innerHTML="E-12, OBH - Palash Nivas, IIIT Hyderabad,<br> Gachibowli, Hyderabad-500032";
     }
     else if ($("#im2").is(":hover")) {
       document.getElementById('description').innerHTML="facebook.com/harshavardhan.bandi.1";
     }
     else if ($("#im3").is(":hover")) {
-      document.getElementById('description').innerHTML="bandiharshav97@gmail.com, harsha.bandi@students.iiit.ac.in";
+      document.getElementById('description').innerHTML="bandiharshav97@gmail.com,<br> harsha.bandi@students.iiit.ac.in";
     }
     else if ($("#im4").is(":hover")) {
       document.getElementById('description').innerHTML="twitter.com/harsha_bandi";
@@ -19,6 +19,6 @@ function onHover()
       document.getElementById('description').innerHTML="bitbucket.org/infinitearray";
     }
     else if ($("#im7").is(":hover")) {
-      document.getElementById('description').innerHTML="+91 9573533879, +91 9885900595";
+      document.getElementById('description').innerHTML="+91 9573533879,<br> +91 9885900595";
     }
 };
